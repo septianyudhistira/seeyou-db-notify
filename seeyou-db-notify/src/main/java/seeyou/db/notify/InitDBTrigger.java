d5a0efb0a4dd56cf2d239fee44a9a15df8878297
septianyudhistira <septian.yudhistira.1402@gmail.com>
2025-01-07 17:27:06 +0700

    Init

--- seeyou-db-notify/src/main/java/seeyou/db/notify/InitDBTrigger.java
+++ seeyou-db-notify/src/main/java/seeyou/db/notify/InitDBTrigger.java
@@ -13,13 +13,13 @@ import java.util.Map;
  * @since 2025-01-06
  */
 public class InitDBTrigger {
-    public static String DB_NOTIFY_NAME = "balantika_db_notify";
+    public static String DB_NOTIFY_NAME = "seeyou_db_notify";
 
     public void createTrigger(Database database) throws Exception {
-        String functionName = "balantika_notify_all_table";
-        String dbNotifyName = database.getValue("SELECT n.nspname AS function_schema, p.proname AS function_name FROM pg_proc p LEFT JOIN pg_namespace n ON p.pronamespace = n.oid WHERE n.nspname NOT IN ('pg_catalog', 'information_schema') AND p.proname = 'balantika_notify_all_table' ");
+        String functionName = "seeyou_notify_all_table";
+        String dbNotifyName = database.getValue("SELECT n.nspname AS function_schema, p.proname AS function_name FROM pg_proc p LEFT JOIN pg_namespace n ON p.pronamespace = n.oid WHERE n.nspname NOT IN ('pg_catalog', 'information_schema') AND p.proname = 'seeyou_notify_all_table' ");
         if (dbNotifyName == null || dbNotifyName.isEmpty()) {
-            String templateFunction = "CREATE OR REPLACE FUNCTION balantika_notify_all_table()\nRETURNS trigger AS $$\nDECLARE\n  current_row RECORD;\nBEGIN\n  IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN\n    current_row := NEW;\n  ELSE\n    current_row := OLD;\n  END IF;\n  PERFORM pg_notify(\n    '" + DB_NOTIFY_NAME + "',\n    json_build_object(\n      'p_tbl', TG_TABLE_NAME,\n      'p_id', current_row.id,\n      'p_type', TG_OP,\n\t  'p_datetime', now()::timestamp(6)\n    )::text\n  );\n  RETURN current_row;\nEND;\n$$ LANGUAGE plpgsql;\n\n";
+            String templateFunction = "CREATE OR REPLACE FUNCTION seeyou_notify_all_table()\nRETURNS trigger AS $$\nDECLARE\n  current_row RECORD;\nBEGIN\n  IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN\n    current_row := NEW;\n  ELSE\n    current_row := OLD;\n  END IF;\n  PERFORM pg_notify(\n    '" + DB_NOTIFY_NAME + "',\n    json_build_object(\n      'p_tbl', TG_TABLE_NAME,\n      'p_id', current_row.id,\n      'p_type', TG_OP,\n\t  'p_datetime', now()::timestamp(6)\n    )::text\n  );\n  RETURN current_row;\nEND;\n$$ LANGUAGE plpgsql;\n\n";
             database.executeSql(templateFunction);
             MyLogger.dbNotify("Create function : " + DB_NOTIFY_NAME);
         }
@@ -43,7 +43,7 @@ public class InitDBTrigger {
             } while(dbTrgName != null && !dbTrgName.isEmpty());
 
             MyLogger.dbNotify("Create Trigger : " + triggerName);
-            String templateTrigger = this.getTableTrigger(triggerName, tableName, "balantika_notify_all_table");
+            String templateTrigger = this.getTableTrigger(triggerName, tableName, "seeyou_notify_all_table");
             database.executeSql(templateTrigger);
         }
     }
